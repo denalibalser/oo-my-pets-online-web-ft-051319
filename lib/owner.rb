f5c06f43b23bd412be5beeec332880b82278382f
Denali Balser <denali.balser167@gmail.com>
2019-05-28 14:56:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,7 @@ class Owner
   end
 
   def sell_pets
-    @@pets.collect do |species, instances|
+    @@pets.each do |species, instances|
       instances.each do |pet|
         pet.mood = 'nervous'
         binding.pry
